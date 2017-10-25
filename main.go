2bce5db3981283a96d686483d329bdc78caad1e1
James Langford <derivative7117@live.com>
2017-10-25 00:59:32 -0600

    added react front end

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
-
 import (
 	"log"
 	"net/http"
 )
 
-
 func main() {
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/",fs)
+	http.Handle("/", fs)
 	log.Println("Hello World!!!")
 	http.ListenAndServe(":3525", nil)
 }
-
